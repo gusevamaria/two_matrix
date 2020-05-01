f28dd16c1782fe69e8de0cf188f94d0f3a6d315f
gusevamaria <60210657+gusevamaria@users.noreply.github.com>
2020-05-01 21:45:53 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SESSION_CONFIGS = [
         'num_demo_participants': 3,
         'app_sequence': ['realefforttask'],
         'task': 'TwoMatrices',
-        'task_params': {'difficulty': 5},
+        'task_params': {'difficulty': 10},
     }
 ]
 
