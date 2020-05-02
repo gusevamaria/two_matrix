82cfec7bafd6d1edb8c60590ac410cb55d7f78e8
GusevAlexander <alexander.gus@gmail.com>
2020-05-03 00:53:15 +0600

    Commit Test_version_8players

--- settings.py
+++ settings.py
@@ -55,11 +55,11 @@ ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 # Consider '', None, and '0' to be empty/false
-#DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
-if environ.get('OTREE_PRODUCTION') not in {None, '', '0'}:
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
+# if environ.get('OTREE_PRODUCTION') not in {None, '', '0'}:
+#     DEBUG = False
+# else:
+#     DEBUG = True
 
 
 DEMO_PAGE_INTRO_HTML = """ """
